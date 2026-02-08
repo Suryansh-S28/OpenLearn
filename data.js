bd67365cd32974b6d098a99b9111238267e75824
Suryansh-S28 <suryanshgupt7@gmail.com>
2026-02-08 21:02:06 +0530

    push

--- data.js
+++ data.js
@@ -3,18 +3,43 @@ const courses = [
     title: "Java Course",
     slug: "java",
     keywords: ["java", "java course", "core java"],
-    page: "./courses/java.html"
+    page: "courses/java.html"
   },
   {
     title: "Python for Beginners",
     slug: "python",
     keywords: ["python", "python course", "learn python"],
-    page: "./courses/python.html"
+    page: "courses/python.html"
   },
   {
     title: "Machine Learning Basics",
     slug: "ml",
     keywords: ["machine learning", "ml basics", "intro to ml"],
-    page: "./courses/ml.html"   
+    page: "courses/ml.html"   
+  },
+  {
+    title: "c++ Programming",
+    slug: "cpp",
+    keywords: ["c++", "c++ programming", "cpp"],
+    page: "courses/cpp.html"
+  },
+  {
+    title: "Data Science",
+    slug: "data-science",
+    keywords: ["data science", "data analysis", "statistics"],
+    page: "courses/data-science.html"
+  },
+  {
+    title: "Web design",
+    slug: "web-dev",  
+    keywords: ["web design", "html", "css", "javascript"],
+    page: "courses/web-design.html"
+  },
+  {
+    title: "mobile app development",
+    slug: "mobile-app-dev",
+    keywords: ["mobile app", "app development", "android", "ios"],
+    page: "courses/mobile-app-dev.html"
   }
+  
 ];
